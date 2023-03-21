d141bde84d3754f4d98dfa65ede22865b9b46fa7
szczypkakrzys <kszczypka21@gmail.com>
2023-03-21 23:50:54 +0100

    vehicle controller

---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,21 @@
+﻿using BikeRental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeRental.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //simple db just for now ;)
+        private List<Vehicle> VehiclesList = new List<Vehicle>()
+        {
+            new Vehicle {Id=1, isElectric=false, Description="Korzystając z doświadczenia kolarzy powstał KROSS Level 11.0. Prawdziwa maszyna, której przeznaczeniem jest agresywna jazda w terenie czy walka na kresce.", rentCost= 200},
+            new Vehicle {Id=2 , isElectric=true, Description="Rower elektryczny to nasza propozycja dla osób poszukających nowych wyzwań i oczekujących od roweru czegoś „ekstra”. Zbudowany zgodnie ze wszystkimi standardami wymaganymi obecnie od rowerów górskich typu trail i w oparciu o wydajny oraz aktywnie wspomagający rowerzystę silnik Shimano STEPS. Dzięki temu na szczyt dotrzesz szybciej i mniej zmęczony, pozostawiając sobie więcej czasu na niczym nieskrępowaną jazdę w dół.", rentCost=300},
+            new Vehicle {Id=3, isElectric=true, Description="Podstawowy model roweru hardtail wyposażonego we wspomaganie elektryczne. Ten e-bike to doskonały wybór dla każdego chcącego sprawdzić swoich sił w nowej dyscyplinie lub poszukującego roweru do objechania trasy wyścigu w komfortowych warunkach bez nadmiernego zmęczenia organizmu przed ważnym startem.", rentCost=250}
+        };
+    }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace BikeRental.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool isElectric { get; set; }
+        public double rentCost { get; set; }
+    }
+}
