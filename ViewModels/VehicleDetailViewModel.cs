9dd3298a27321a1960f0ae0810c718957bbedb1a
szczypkakrzys <kszczypka21@gmail.com>
2023-03-22 23:25:14 +0100

    untrack files from .gitignore

--- ViewModels/VehicleDetailViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using BikeRental.Models;
-
-namespace BikeRental.ViewModels
-{
-    public class VehicleDetailViewModel
-    {
-       public Vehicle VehicleDetails { get; set; }
-    }
-}
--- ViewModels/VehicleItemViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using BikeRental.Models;
-
-namespace BikeRental.ViewModels
-{
-    public class VehicleItemViewModel
-    {
-        public List<Vehicle> VehiclesList { get; set; }
-    }
-}
