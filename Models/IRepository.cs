328873f4620a6a14675c3bd4ad66d387e4a7684d
szczypkakrzys <kszczypka21@gmail.com>
2023-04-04 21:29:20 +0200

    Generic repository interface

---
+++ Models/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BikeRental.Models
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> GetAllRecords();
+        T GetSingle(Guid id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+        void Save();
+
+    }
+}
