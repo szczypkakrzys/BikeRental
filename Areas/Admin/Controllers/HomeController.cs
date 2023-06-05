b48018fb52977b53dbff877b9fbf41531ff88a09
szczypkakrzys <kszczypka21@gmail.com>
2023-06-05 19:47:56 +0200

    Admin Panel continuation

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BikeRental.DAL;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BikeRental.Models;
+using BikeRental.ViewModels;
 
 namespace BikeRental.Areas.Admin.Controllers
 {
@@ -8,6 +13,13 @@ namespace BikeRental.Areas.Admin.Controllers
     [Authorize(Roles = "Administrator")]
     public class HomeController : Controller
     {
+        private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
+        public HomeController(DatabaseContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
         // GET: HomeController
         public IActionResult Index()
         {
@@ -18,6 +30,25 @@ namespace BikeRental.Areas.Admin.Controllers
         {
             return View();
         }
-        
+        [Route("Admin/Users")]
+        public IActionResult Users()
+        {
+            var usersList = _context.Users.ToList();
+            return View(usersList);
+        }       
+        [HttpPost]
+        public async Task<IActionResult> OperatorRole(Guid id)
+        {
+            User user = await _userManager.FindByIdAsync(id.ToString());
+            await _userManager.AddToRoleAsync(user, "Operator");
+            return RedirectToAction("Admin/Users");
+        }
+        //public async Task<IList <string>> Role(Guid id)
+        //{
+        //    var user = await _userManager.FindByIdAsync(id.ToString());
+        //    var role = await _userManager.GetRolesAsync(user);
+            
+        //    return role;
+        //}
     }
 }
